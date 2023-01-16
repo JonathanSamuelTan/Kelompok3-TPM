1a2d33bc00a6866274131819ee4446e5fbdabbb2
JonathanSamuelTan <jonathansam215@gmail.com>
2023-01-16 21:01:00 +0700

    Sam : Login system + CSS & JS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,54 +1,68 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        {{-- <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div> --}}
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Group Name')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login to your account</title>
+    <link rel="stylesheet" href="{{asset('css/Login.css')}}">
+</head>
+<body>
+    <section id="login-page">
+        <div class="page-1">
+            <div class="text">
+                <h2>Welcome Back</h2>
+                <p>Please enter the information you entered while registering</p>
+            </div>
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+            <form id="login-form" method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="input-form">
+                    <label for="Nama-Team">Nama Team</label>
+                    <input type="text" name="name" id="Nama-Team" :value="old('name')" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+                <div class="input-form password">
+                    <label for="Passowrd-login">Password</label>
+                    <input type="password" name="password" id="Passowrd-login" required autocomplete="current-password"/>
+                    <span onclick="showPassword()">
+                        <iconify-icon icon="ph:eye-closed-duotone" class="hidden-icon" id="hide1"></iconify-icon>
+                        <iconify-icon icon="pepicons-pop:eye" class="hidden-icon" id="hide2"></iconify-icon>
+                    </span>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                    <div class="button-login">
+                        <button type="submit" id="login">{{ __('Log in') }}</button>
+                    </div>
+                </div>
+        
+                <div class="Regis-ulang">
+                    <p style="color: #707070
+                    ">Belum memiliki akun?</p> 
+                    <a href="Informastion-Group.html">
+                    <p style="color: #0D4C92
+                    "> Register disini</p>
+                    </a>
+                </div>
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="logo-regis">
+            <img src="assets/logoRegis.png" width="205.74px" height="29.87px" alt="LogoTechnoscape2023">
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+    <script src="{{asset('js/Login.js')}}"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
+</body>
+</html>
